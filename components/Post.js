fca47d902a6e92b5bc9aadc1027f9cb57ebe690e
IsomerX <49413460+IsomerX@users.noreply.github.com>
2022-04-21 11:00:08 +0530

    updates

--- components/Post.js
+++ components/Post.js
@@ -4,11 +4,21 @@ import React from "react";
 const Post = ({ id, post, postPage }) => {
     return (
         <div className="p-3 flex cursor-pointer border-b border-gray-700">
-            {!postPage && <img src={post?.userImg} alt="" className="h-11 w-11 rounded-full mr-4"/>}
+            {!postPage && (
+                <img
+                    src={post?.userImg}
+                    alt=""
+                    className="h-11 w-11 rounded-full mr-4"
+                />
+            )}
             <div className="flex flex-col space-y-2 w-full">
                 <div className={`flex ${!postPage && "justify-between"}`}>
                     {postPage && (
-                        <img src={post?.userImg} alt="" />
+                        <img
+                            src={post?.userImg}
+                            alt=""
+                            className="h-11 w-11 rounded-full mr-4"
+                        />
                     )}
                 </div>
             </div>
