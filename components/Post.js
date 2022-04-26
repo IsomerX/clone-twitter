565b0c602749ae41900e8b5a99e53c211c46559f
IsomerX <49413460+IsomerX@users.noreply.github.com>
2022-04-26 19:19:11 +0530

    new

--- components/Post.js
+++ components/Post.js
@@ -11,15 +11,44 @@ const Post = ({ id, post, postPage }) => {
                     className="h-11 w-11 rounded-full mr-4"
                 />
             )}
+            {console.log(post)}
             <div className="flex flex-col space-y-2 w-full">
                 <div className={`flex ${!postPage && "justify-between"}`}>
                     {postPage && (
                         <img
                             src={post?.userImg}
-                            alt=""
+                            referrerpolicy="no-referrer"
+                            alt="Profile Pic"
                             className="h-11 w-11 rounded-full mr-4"
                         />
                     )}
+                    <div className="text-[#6e767d]">
+                        <div className="inline-block group">
+                            <h4
+                                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
+                                    !postPage && "inline-block"
+                                }`}
+                            >
+                                {post?.username}
+                            </h4>
+                            <span
+                                className={`text-sm sm:text-[15px] ${
+                                    !postPage && "ml-1.5"
+                                }`}
+                            >
+                                @{post?.tag}
+                            </span>
+                        </div>{" "}
+                        ·{" "}
+                        <span className="hover:underline text-sm sm:text-[15px]">
+                            {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+                        </span>
+                        {!postPage && (
+                            <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+                                {post?.text}
+                            </p>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
