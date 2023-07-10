e912613b992cb6687d8ca63992040443ba839610
isomerx <dhruvbakshi12343@gmail.com>
2023-07-10 06:20:22 +0530

    package update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  images:{
-    domains: [
-      "rb.gy",
-      "rb.gy/ogau5a",
-      "lh3.googleusercontent.com"
-    ]
-  }
-}
+    images: {
+        domains: ["rb.gy", "rb.gy/ogau5a", "lh3.googleusercontent.com"],
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
